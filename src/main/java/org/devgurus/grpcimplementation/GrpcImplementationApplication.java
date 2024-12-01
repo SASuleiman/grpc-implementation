4ab009c2a0b1d325695f72b9b0bc4ee978ad5c4c
Suleiman Suleiman <suleimansuleiman606@gmail.com>
2024-12-01 13:44:32 +0100

    Project_Structure

---
+++ src/main/java/org/devgurus/grpcimplementation/GrpcImplementationApplication.java
@@ -0,0 +1,13 @@
+package org.devgurus.grpcimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devgurus/grpcimplementation/GrpcImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devgurus.grpcimplementation;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GrpcImplementationApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
